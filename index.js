0b8071cc72d28bad4f95db466dc7aac6c590f3df
cste14 <emonnstu14@gmail.com>
2022-10-06 23:44:48 +0600

    index

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 
 //db
-const url = process.env.DB_URL;
+const url = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.tfzr2.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
 mongoose
   .connect(url, {
     useNewUrlParser: true,
