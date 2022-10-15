597adf4bd9658c7d17498f910c22c9fd974ca958
cste14 <emonnstu14@gmail.com>
2022-10-16 00:58:29 +0600

    edit commnet section

--- model/NewsCommetModel.js
+++ model/NewsCommetModel.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
   commentId: { type: String, required: true },
   comment: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  photo: { type: String, required: true },
 });
 const NewsComment = mongoose.model("comments", commentSchema);
 module.exports = NewsComment;
